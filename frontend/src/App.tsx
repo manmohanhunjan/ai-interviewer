6ab6fe6cef1897ec0b4477a77b17133d9729c84a
manmohanhunjan <71703296+manmohanhunjan@users.noreply.github.com>
2025-05-23 15:20:22 +0530

    new screens linkGenerator InterviewScreen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { InterviewScreen } from './InterviewScreen'
 import Landing from './Landing'
+import { LinkGenerator } from './LinkGenerator'
 
 function App() {
   return (
     <div>
       <Landing />
+      <LinkGenerator />
+      <InterviewScreen />
     </div>
   )
 }
